c4486f7e9bcbfd2d944f4af054e1a1a898fe0abb
fabio <fabio.forenza@gmail.com>
2024-01-09 19:46:02 +0100

    secondo esercizio

---
+++ secondoesercizio.php
@@ -0,0 +1,10 @@
+<?php
+$text = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$te_xt6 = $text2;
+$text7 = 'bevuto';
+$text8 = "tutto";
+echo $text . " " .$text2. " " .$text3.$text4. " " .$text5. " " .$te_xt6. " " .$text7. " " .$text8.".";
