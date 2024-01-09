f0680f0b6b9420562a5ce5546bb531a09e44a9b6
fabio <fabio.forenza@gmail.com>
2024-01-09 20:47:37 +0100

    esercizio extra

---
+++ php-1/esercizioextra.php
@@ -0,0 +1,7 @@
+<?php
+$corsoHackademy = [ "docenti" => ["daniele", "emanuele", "Mattia"], "studenti" => ["Antonio",
+"franco", "Fabio", "Domenico"], "argomenti" => ["html", "css", "bootstrap", "javascript", "php"], ];
+
+$result = "Sono " .$corsoHackademy['studenti'][2] ." e sto svolgendo gli esercizi con " .$corsoHackademy['studenti'][3] 
+." ed il nostro professore si chiama " .$corsoHackademy['docenti'][1];
+ echo $result;
