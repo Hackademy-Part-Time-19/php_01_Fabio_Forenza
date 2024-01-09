f85044649a70dde35b36140037195e648fb8d619
fabio <fabio.forenza@gmail.com>
2024-01-09 19:35:51 +0100

    primo esercizio

---
+++ php-1/index.php
@@ -0,0 +1,11 @@
+<?php
+$integer = 4;
+$float = 3.5;
+$stringa = "sono una stringa";
+$boolean = true;
+$boolean2 = false; 
+var_dump ($integer);
+var_dump ($float);
+var_dump ($stringa);
+var_dump ($boolean);
+var_dump ($boolean2);
