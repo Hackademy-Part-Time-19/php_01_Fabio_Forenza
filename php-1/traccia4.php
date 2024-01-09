25d3b85dfea9eb97f77acc8594ed431d1960f075
fabio <fabio.forenza@gmail.com>
2024-01-09 20:30:02 +0100

    quarto esercizio

---
+++ php-1/traccia4.php
@@ -0,0 +1,14 @@
+<?php
+$x = 10;
+$y = 20;
+$z = 20;
+$w = 10;
+
+var_dump($x > $y);
+var_dump($z < $w);
+var_dump($x >= $z);
+var_dump($w <= $y);
+var_dump($y === $w);
+var_dump($x !== $w);
+var_dump($y == $z);
+var_dump($z != $w);
